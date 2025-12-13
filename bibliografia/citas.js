34f40fdb8519699832872dc1bc6d5d1ffb297d0c
Olimar <gpfernandezflorio@gmail.com>
2025-12-13 12:00:36 -0300

    Bibliografía: separo el archivo principal en varios archivos y agrego algunas listas de etiquetas

---
+++ bibliografia/citas.js
@@ -0,0 +1,51 @@
+const citas = {
+  // Sobre enseñar ciencia
+  "en ciencia es importante usar la terminología correcta y enseñar a usar esa terminología":['Lemke1997'],
+  "hay que conocer las concepciones de les estudiantes, que salgan a la luz":['Lemke1997'],
+  "no hay que enseñar el método científico, que las teorías son verdaderas o falsas ni que estas se demuestran a partir de observaciones objetivas; las teorías se usan mientras son útiles, hasta que aparezca otra más útil":['Lemke1997'],
+  "hay que humanizar la ciencia hablando de las personas; es peligroso que crean que son personas con inteligencia sobrehumana y los disuade de estudiar estas carreras":['Lemke1997'],
+  // Estrategias didácticas
+  "mejor que les estudiantes discutan en grupos en lugar del 'diálogo triádico' (le docente habla y les estudiantes sólo interrumpen para preguntar)":['Lemke1997'],
+  // Sobre enseñar a programar
+  "enseñar a programar es difícil":['onTheDifficulty'],
+  "la sintaxis dura puede ser poco intuitiva y frustrante para principiantes":["scratchJr","ScratchForCS"],
+  "los lenguajes de alto nivel hacen la programación más accesible, por ser más parecidos al lenguaje natural":["fromImperativeToVisual"],
+  "tienen que enseñar quienes saben enseñar a programar y no quienes saben programar":['onTheDifficulty'],
+  // Enfoques para enseñar
+  "enfoque espiralado para separar sintaxis y semántica":["spiralApproach"],
+  // Lenguajes para enseñar
+  "el lenguaje es un vehículo pero el objetivo no es enseñar el lenguaje sino a programar":['onTheDifficulty'],
+  "quienes aprenden con python cometen menos errores que quienes aprenden con java":["SimpleLanguage"],
+  "la principal justificación para elegir un lenguaje en CS1 es 'beneficio pedagógico'":["surveyInPr"],
+  // Elección del lenguaje
+  "hay que elegir un lenguaje pedagógico para enseñar y no uno popular":['onTheDifficulty'],
+  "conviene enseñar con un lenguaje más simple al principio":["SimpleLanguage"],
+  "Esfuerzo adicional para enseñar ciertos conceptos a partir de las características del lenguaje usado":['massa_2023'],
+  // Transferencia
+  "al aprender un segundo lenguaje, sólo hay que aprender la sintaxis pero la semántica ya es sabida (a menos que el segundo lenguaje tenga nuevas estructuras semánticas)":["spiralApproach"],
+  "transferencia exitosa Python -> Java":["SimpleLanguage"],
+  // Éxito de los bloques
+  "actitud positiva hacia los bloques (no necesariamente aprendieron algo)":["ScratchForCS"],
+  "usando Alice mejoraron las notas":["newInstructionalApproach"],
+  // Ventajas de los bloques
+  "las interfaces visuales permiten modelar ideas complejas":["blocksVsText"],
+  // Fallas en los bloques
+  "percepción de que los bloques no son programación real":[
+    {fromImperativeToVisual:'several students remarked that Alice seemed "simplistic", and to bear little relationship to "real programming".'}
+  ],
+  "el contenido visual hace que parezca de juguete":["blocksVsText"],
+  // Transferencia bloques -> texto
+  "aprender bloques facilitó aprender texto":[
+    {ScratchForCS:"Though we did not learn Java syntax by using Scratch, we learned the type of thinking necessary to implement simple programs. [...] I was able to approach the first Java programs with an idea of how to tackle the problems. Though I did not yet know how to create a for loop, I knew when a for loop was necessary because I had used loops in my Scratch program."},
+    "textBasedElementary"
+  ],
+  "el conocimiento adquirido en bloques es difícil de transferir a la práctica":["blocksVsText"],
+  // Transferencia PC a otras áreas
+  "no hay estrategias generales de resolución de problemas, hay específicas de cada área y no se pueden transferir trivialemnte (principalmente entre Cs naturales y sociales pero también dentro de ellas)":["Pozo1994"],
+  // Historia de la computación
+  "descripción de las Máquinas de Turing":["Turing1936","Turing1938"],
+  // Elección de carrera
+  "presentar a la ciencia deshumanizada (objetiva e imparcial) o realizada por personas con inteligencia sobrehumana disuade de estudiar estas carreras":['Lemke1997'],
+  // Resolución de problemas
+  "que sea un problema o un ejercicio depende de la experiencia del estudiante (algo que para un experto es un ejercicio puede ser un problema para un novato)":["Pozo1994"]
+};
---
+++ bibliografia/conferenciasYRevistas.js
@@ -0,0 +1,217 @@
+bib.conferencias = {
+  'JADICC':{T:"Jornadas Argentinas de Didáctica de las Ciencias de la Computación", short:"JADICC",
+    2023:{T:"%T 2023"},
+    2024:{T:"%T 2024"}
+  },
+  'SIGCSE':{T:"ACM Technical Symposium on Computer Science Education", c:"ACM", short:"SIGCSE",
+    2003:{T:"Proceedings of the 34th %T",f:{a:2003},l:"Reno, Nevada, USA"},
+    2004:{T:"Proceedings of the 35th %T",f:{a:2004,m:3,d:"3-7"},l:"Norfolk, Virginia"},
+    2005:{T:"Proceedings of the %T",f:{a:2005},l:"St. Louis, Missouri"},
+    2007:{T:"Proceedings of the 38th %T",f:{a:2007,m:3},l:"Covington, Kentucky, USA"},
+    2008:{T:"Proceedings of the 39th %T",f:{a:2008,m:3},l:"Portland, OR",short:"SIGCSE '08"},
+    2009:{T:"%T",f:{a:2009},f:{m:3, a:2009},l:"Chattanooga, TN"},
+    2010:{T:"Proceedings of the 41st %T",f:{a:2010},l:'New York, NY'},
+    2011:{T:"Proceedings of the 42nd %T",f:{a:2011,m:3},l:'Dallas, TX'},
+    2012:{T:"Proceedings of the 43rd %T",f:{a:2012,m:3},l:'Raleigh, NC, USA'},
+    2013:{T:"Proceedings of the 44th %T",f:{a:2013,m:3}},
+    2014:{T:"Proceedings of the 45th %T",f:{a:2014},l:'New York, NY, USA'},
+    2015:{T:"Proceedings of the 46th %T",f:{a:2015},l:"New York, NY, USA"},
+    2016:{T:"Proceedings of the 47th %T",f:{a:2016},l:"Memphis, TN, USA"},
+    2017:{T:"Proceedings of the 2017 %T",f:{a:2017}},
+    2018:{T:"Proceedings of the 49th %T",f:{a:2018},l:"Baltimore, MD, USA"},
+    2020:{T:"Proceedings of the 51st %T",f:{a:2020},l:"Portland, OR, USA"},
+    2021:{T:"Proceedings of the 52nd %T",f:{a:2021,m:3}},
+    2024:{T:"Proceedings of the 55th %T",f:{a:2024,m:3,d:"20-23"},l:"Portland, OR, USA"}
+  },
+  'ICER':{T:"Computing Education Research", c:"ACM", short:"ICER",
+    '08':{T:"Proceedings of the ACM Workshop on International %T",f:{a:2008,m:9}},
+    WkSp:{T:"International Workshop on %T",
+      2005:{T:"Proceedings of the 2005 %T",f:{a:2005},l:'New York, USA'},
+      2006:{T:"Proceedings of the 2nd %T",f:{a:2006}},
+      2007:{T:"Proceedings of the third %T",f:{a:2007}},
+      2008:{T:"Proceedings of the Fourth %T",f:{a:2008},l:'New York, NY, USA'},
+      2010:{T:"Proceedings of the Sixth %T",f:{a:2010},l:'New York, NY, USA'}
+    },
+    CONF:{T:"Annual International ACM Conference on %T",
+      2013:{T:"Proceedings of the Ninth %T",f:{a:2013},l:'New York, New York, USA'},
+      2015:{T:"Proceedings of the Eleventh %T",f:{a:2015}}
+    },
+    2016:{T:"Proceedings of the 2016 ACM Conference on International %T",f:{a:2016}},
+    2021:{T:"Proceedings of the 17th ACM Conference on International %T",f:{a:2021},l:'Virtual Event, USA'}
+  },
+  'ITiCSE':{T:"Innovation and technology in computer science education", c:"ACM", short:"ITiCSE",
+    CONF:{T:"Conference on %T",
+      2000:{T:"Proceedings of the 5th annual %T",f:{a:2000,m:7},l:"Canterbury, England"},
+      2001:{T:"Proceedings of the 6th annual %T",f:{a:2001},l:"Canterbury, England"},
+      2003:{T:"Proceedings of the 8th annual %T",f:{a:2003},l:"Thessaloniki, Greece"},
+      2010:{T:"Proceedings of the 15th %T",f:{a:2010}},
+      2011:{T:"Proceedings of the 16th annual joint %T",f:{a:2011},l:'New York, NY, USA'},
+      2012:{T:"Proceedings of the %T",f:{a:2012}},
+      2013:{T:"Proceedings of the 18th annual joint %T",f:{a:2013}},
+      2014:{T:"Proceedings of the 2014 %T",f:{a:2014},l:"NY, USA"},
+      2015:{T:"Proceedings of the 2015 %T",f:{a:2015},l:"New York"},
+      2016:{T:"Proceedings of the 2016 ACM annual joint %T",f:{a:2016,m:7,d:"9-13"},l:'Arequipa, Peru'},
+      2018:{T:"Proceedings Companion of the 23rd Annual ACM Conference on %T",f:{a:2018,m:7},l:"New York, NY, USA"},
+      2019:{T:"Proceedings of the 2019 ACM %T",f:{a:2019},l:"New York, NY, USA"}
+    },
+    WGR:{T:"Working group reports on %T", short:"%S-WGR",
+      2001:{T:"%T 2001", f:{a:2001}, l:"Canterbury, UK"},
+      2014:{T:"Proceedings of the %T",f:{a:2014}},
+      2015:{T:"Proceedings of the %T",f:{a:2015}},
+      2016:{T:"Proceedings of the 2016 %T",f:{a:2016}},
+      2007:{f:{a:2007}}
+    },
+    1999:{T:"Proceedings of ITiCSE '99'",f:{a:1999}},
+    2001:{T:"Proceedings of ITiCSE 2001",f:{a:2001}},
+    2019:{T:"%T",f:{a:2019,m:7,d:"15-17"},l:"Aberdeen, Scotland UK"},
+  },
+  'WiPSCE':{T:"Workshop in Primary and Secondary Computing Education", c:"ACM", short:"WiPSCE",
+    2013:{T:"Proceedings of the 8th %T",f:{a:2013},l:"New York, New York, USA"},
+    2014:{T:"Proceedings of the 9th %T",f:{a:2014},l:"New York, USA"},
+    2015:{T:"Proceedings of the %T",f:{a:2015,m:11},l:"New York, NY, USA"},
+    2017:{T:"Proceedings of the 12th %T",f:{a:2017},l:"Nijmegen, Netherlands"}
+  },
+  'IDC':{T:"International Conference on Interaction Design and Children", c:"ACM", short:"IDC",
+    2005:{T:"Proceedings of %T",f:{a:2005},l:"Boulder, CO"},
+    2013:{T:"Proceedings of the 12th %T",f:{a:2013},l:"New York, NY, USA"},
+    2014:{T:"Proceedings of the 2014 %T",f:{a:2014}},
+    2015:{T:"Proceedings of the 14th %T",f:{a:2015,m:6},l:"New York, NY, USA"},
+    2017:{T:"Proceedings of the 2017 %T",f:{a:2017}},
+    2019:{T:"Proceedings of the 18th ACM %T",f:{a:2019,m:6},l:"New York, NY, USA"}
+  },
+  'IDEE':{T:"Workshop on Interaction Design in Educational Environments", c:"ACM", short:"IDEE",
+    2014:{T:"Proceedings of the 2014 %T",l:"New York, NY, USA"}
+  },
+  'LACLO':{T:"Latin American Conference on Learning Technologies", c:"IEEE", short:"LACLO",
+    2017:{T:"2017 Twelfth %T",f:{a:2017}}
+  },
+  'TE&ET':{T:"Congreso de Tecnología en Educación & Educación en Tecnología", short:"TE&ET",
+    2022: {T:"XVII %T",f:{a:2022,m:6},l:"Entre Ríos, Argentina"}
+  },
+  'HumanFactors':{T:"Proceedings of the SIGCHI Conference on Human Factors in Computing Systems",
+    2009:{f:{a:2009},l:"Boston, MA, USA"}
+  }
+};
+bib.revistas = {
+  'ACMComm':{T:"Communications of the ACM", c:"ACM",
+    11:{3:{f:{a:1968}}},
+    23:{f:{a:1980}},
+    26:{9:{f:{a:1983}}},
+    29:{9:{f:{a:1986}}},
+    32:{1:{f:{a:1989,m:11}},11:{f:{a:1989,m:11}},12:{f:{a:1989}}},
+    49:{3:{f:{a:2006}}},
+    52:{11:{f:{a:2009,m:11}}},
+    60:{6:{f:{a:2009,m:11}}},
+    62:{2:{f:{a:2019}},11:{f:{a:2019,m:10}}}
+  },
+  'TOCE':{T:"ACM Transactions on Computing Education", short:"TOCE",
+    10:{4:{f:{a:2010,m:11}}},
+    12:{2:{f:{a:2012}}},
+    14:{4:{f:{a:2015,m:2}}},
+    22:{4:{f:{a:2022}}},
+    24:{1:{f:{a:2024}}}
+  },
+  'IEEEed':{T:"IEEE Transactions on Education",
+    62:{2:{f:{a:2019}}}
+  },
+  'SIGCSEBull':{T:"ACM SIGCSE Bulletin",
+    28:{3:{f:{a:1996,m:9}}},
+    35:{1:{f:{a:2003,m:1}}},
+    36:{
+      3:{f:{a:2004}},
+      4:{f:{a:2004,m:6}}
+    },
+    38:{
+      1:{f:{a:2006}},
+      3:{f:{a:2006}},
+      4:{f:{a:2006,m:12}}
+    },
+    39:{
+      3:{f:{a:2007}},
+      4:{f:{a:2007}}
+    },
+    40:{3:{f:{a:2008}}},
+    41:{
+      1:{f:{a:2009}},
+      3:{f:{a:2009}}
+    }
+  },
+  'CSinCollege':{T:"Journal of Computing Sciences in Colleges",
+    15:{4:{f:{a:2000}}},
+    24:{3:{f:{a:2009}}},
+    25:{5:{f:{a:2010}}},
+    26:{2:{f:{a:2010}}}
+  },
+  'CSEduc':{T:"Computer Science Education",
+    13:{2:{f:{a:2003}}},
+    16:{3:{f:{a:2006}}},
+    23:{3:{f:{a:2013}}},
+    29:{2:{f:{a:2019}}}
+  },
+  'LearnSc':{T:"Journal of the Learning Sciences",
+    23:{f:{a:2014}}
+  },
+  'IJTDE':{T:"International Journal of Technology and Design Education",
+    32:{f:{a:2022}}
+  },
+  'ResTechEduc':{T:"Journal of Research on Technology in Education",
+    46:{1:{f:{a:2013}}}
+  },
+  'EducTechSys':{T:"Journal of Educational Technology Systems",
+    45:{1:{f:{a:2016}}}
+  },
+  'EducTech':{T:"Educational Technology",
+    52:{4:{f:{a:2012}}}
+  },
+  'EducRes':{T:"Educational researcher",
+    33:{8:{f:{a:2004}}}
+  },
+  'revEducRes':{T:"Review of Educational Research",
+    78:{f:{a:2008}}
+  },
+  'InfoEduc':{T:"Informatics in Education",
+    9:{1:{f:{a:2010}}},
+    13:{1:{f:{a:2014}}},
+    19:{2:{f:2020}},
+    20:{1:{f:2021}}
+  },
+  'EducInfTechnol':{T:"Education and Information Technologies", c:"Springer US",
+    25:{4:{f:{a:2020}}},
+    26:{5:{f:{a:2021,m:9}}},
+    28:{f:{a:2023}},
+    29:{5:{f:{a:2024}}}
+  },
+  'Comp&Educ':{T:"Computers & Education", c:"Elsevier Ltd",
+    1:{4:{f:{a:1977}}},
+    40:{1:{f:{a:2003}}},
+    58:{f:{a:2012},
+    3:{f:{a:2012}}},
+    63:{f:{a:2013}},
+    72:{f:{a:2014}},
+    75:{f:{a:2014}},
+    210:{f:{a:2024,m:3}},
+    216:{f:{a:2024}}
+  },
+  'EducCompRes':{T:"Journal of Educational Computing Research",
+    2:{1:{a:1986}},
+    7:{3:{a:1991}},
+    61:{6:{f:{a:2023}}}
+  },
+  'CompHumanBehave':{T:"Computers in Human Behavior",
+    30:{f:{a:2014}},
+    35:{f:{a:2014}},
+    41:{f:{a:2014}},
+    52:{f:{a:2015,m:11}},
+    72:{f:{a:2017}},
+    109:{f:{a:2020}}
+  },
+  'VirtEducCien':{T:"Virtualidad, Educación y Ciencia",
+    20:{11:{f:{a:2020}},ISSN:'1853-6530'}
+  },
+  'EducADist':{T:"Revista de Educación a Distancia",
+    46:{f:{a:2015}}
+  },
+  'LatAmEduInc':{T:"Revista latinoamericana de educación inclusiva",
+    12:{1:{f:{a:2018,m:4}}}
+  }
+};
